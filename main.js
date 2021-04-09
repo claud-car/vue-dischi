54f81fbee1e077cba793e4b01bc889e9c69fcaa0
Claudio <claudiocarchietti@yahoo.it>
2021-04-09 16:54:48 +0200

    Esercizio terminato,con bonus e aggiunta di una funzionalità
    Aggiunta di un href ad ogni brano,che ti re-indirizza su youtube

--- main.js
+++ main.js
@@ -6,12 +6,65 @@ var app = new Vue({
     //Creato oggetto currentFilter impostato su all
     //per poter visualizzare i brani filtrati
     currentFilter: "All",
-    disks: []
+    disks: [],
+    links:[
+      {
+        title:"Made in Japan",
+        src:  "https://www.youtube.com/watch?v=psFdqiPfhmg",
+        position: 1
+      },
+      {
+        title:"Bad",
+        src:  "https://www.youtube.com/watch?v=Sd4SJVsTulc",
+        position: 2
+      },
+      {
+        title:"New Jersey",
+        src:  "https://www.youtube.com/watch?v=uvhq2mRORFw",
+        position: 3
+      },
+      {
+        title:"And Justice For All",
+        src:  "https://www.youtube.com/watch?v=_fKAsvJrFes",
+        position: 4
+      },
+      {
+        title:"Queen",
+        src:  "https://www.youtube.com/watch?v=-DAvQcP1dlE",
+        position: 5
+      },
+      {
+        title:"Ten's Summoner's Tales",
+        src:  "https://www.youtube.com/watch?v=dG_CypsIupo&list=PLVSmE10fWX-CYGwwuZkffgVE-Wfuiq4XF",
+        position: 6
+      },
+      {
+        title:"Hard Wired",
+        src:  "https://www.youtube.com/watch?v=bL6MhXSUhb4",
+        position: 7
+      },
+      {
+        title:"Brave new World",
+        src:  "https://www.youtube.com/watch?v=X5P_muGUJR4",
+        position: 8
+      },
+      {
+        title:"One more car, one more raider",
+        src:  "https://www.youtube.com/watch?v=ZoDrcK1TI68&list=PLQL8vLGlM3ml_q9s-Xny-dr5mzDL5VosQ",
+        position: 9
+      },
+      {
+        title:"Steve Gadd Band",
+        src:  "https://www.youtube.com/watch?v=_fisshGVQuQ&list=PLULG1QJdgkY7WZFUT2SIJ4sVD_XhZKw-N",
+        position: 10
+      }
+    ]
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((response) => {
       this.disks = response.data.response;
+      console.log(this.disks);
     })
   },//fine mounted
   methods: {
